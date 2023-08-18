bf0baa712ee3e6bca3c544020a1b24f2fe833c52
Nijo Fukushi(aka ClarkKeyton) <141921641+ClarkKeyton@users.noreply.github.com>
2023-08-18 16:20:14 +0300

    Add files via upload

---
+++ vs2022_helpful.bat
@@ -0,0 +1,7 @@
+mklink /j "C:\Program Files (x86)\Microsoft SDKs" "E:\Program Files (x86)\Microsoft SDKs"
+mklink /j "C:\Program Files (x86)\Microsoft Visual Studio\Shared" "E:\Program Files (x86)\Microsoft Visual Studio\Shared"
+mklink /j "C:\Program Files (x86)\Windows Kits" "E:\Program Files (x86)\Windows Kits"
+mklink /j "C:\ProgramData\Microsoft\VisualStudio" "E:\VSSetupCache"
+mklink /j "C:\ProgramData\Package Cache" "E:\CachePackage"
+timeout 5
+exit
